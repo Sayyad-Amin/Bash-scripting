87c973af72a93df847ef438e253218211314bdbc
Muhammad Sayyad Amin <97822048+Sayyad-Amin@users.noreply.github.com>
2025-02-25 13:50:38 +0500

    Update array-demo-02.sh

--- array-demo-02.sh
+++ array-demo-02.sh
@@ -20,3 +20,23 @@ echo "Elements at position 2nd and 3rd are ${Fruits[*]:1:2}."  # Starts from ind
 Fruits+=("Banana" "Grapes")
 echo "Updated Fruits array look like now ${Fruits[*]}."
 
+# Enable associative arrays very useful concept because we can't remember index values so we add key-value pairs for our ease very simple.
+declare -A PERSON
+
+# Adding key-value pairs
+PERSON[name]="Shehzad Khan"
+PERSON[age]=24
+PERSON[country]="Pakistan"
+
+# Accessing values
+echo "Name: ${PERSON[name]}"
+echo "Age: ${PERSON[age]}"
+echo "Country: ${PERSON[country]}"
+
+# Updating a value
+PERSON[age]=25
+echo "Updated Age: ${PERSON[age]}"
+
+
+
+
