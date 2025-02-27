4f08740c9b27b298fb1af89d97b6e922a7c6239f
Muhammad Sayyad Amin <97822048+Sayyad-Amin@users.noreply.github.com>
2025-02-27 10:12:57 +0500

    Update until_loop-11.sh

--- until_loop-11.sh
+++ until_loop-11.sh
@@ -1,4 +1,5 @@
 # Use Case: Similar to while, but stops when the condition becomes true.
+# An until loop runs until a condition becomes true. It is the opposite of a while loop, which runs while a condition is true.
 
 #!/bin/bash
 
