de745c39864288af2f0192d150bf5feb160adce9
Muhammad Sayyad Amin <97822048+Sayyad-Amin@users.noreply.github.com>
2025-02-27 10:28:10 +0500

    Create functions-13.sh

---
+++ functions-13.sh
@@ -0,0 +1,13 @@
+# A function in Bash is a block of code that you can reuse multiple times. It helps avoid repetition and makes scripts organized and readable.
+
+----------------------------------
+
+#!/bin/bash
+
+# Define function
+say_hello() {
+    echo "Hello, World!"
+}
+
+# Call function
+say_hello
